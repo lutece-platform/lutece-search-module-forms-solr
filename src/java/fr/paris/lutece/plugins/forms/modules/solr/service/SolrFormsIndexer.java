39d4c654eb654e20ef6feb589d8be038c83ac1f4
Felix Dupriez <felix.dupriez@accenture.com>
2022-01-04 10:53:05 +0100

    Code simplified and use of methods from FormResponseHome to improve performance

--- src/java/fr/paris/lutece/plugins/forms/modules/solr/service/SolrFormsIndexer.java
+++ src/java/fr/paris/lutece/plugins/forms/modules/solr/service/SolrFormsIndexer.java
@@ -54,7 +54,6 @@ import fr.paris.lutece.plugins.forms.business.FormResponse;
 import fr.paris.lutece.plugins.forms.business.FormResponseHome;
 import fr.paris.lutece.plugins.forms.business.FormResponseStep;
 import fr.paris.lutece.plugins.forms.business.form.search.FormResponseSearchItem;
-import fr.paris.lutece.plugins.forms.service.FormsPlugin;
 import fr.paris.lutece.plugins.forms.service.entrytype.EntryTypeDate;
 import fr.paris.lutece.plugins.forms.service.entrytype.EntryTypeGeolocation;
 import fr.paris.lutece.plugins.forms.service.entrytype.EntryTypeNumbering;
@@ -64,24 +63,18 @@ import fr.paris.lutece.plugins.genericattributes.service.entrytype.EntryTypeServ
 import fr.paris.lutece.plugins.genericattributes.service.entrytype.IEntryTypeService;
 import fr.paris.lutece.plugins.leaflet.business.GeolocItem;
 import fr.paris.lutece.plugins.search.solr.business.field.Field;
-import fr.paris.lutece.plugins.search.solr.business.indexeraction.SolrIndexerAction;
-import fr.paris.lutece.plugins.search.solr.business.indexeraction.SolrIndexerActionHome;
 import fr.paris.lutece.plugins.search.solr.indexer.SolrIndexer;
 import fr.paris.lutece.plugins.search.solr.indexer.SolrIndexerService;
 import fr.paris.lutece.plugins.search.solr.indexer.SolrItem;
 import fr.paris.lutece.plugins.workflowcore.business.state.State;
 import fr.paris.lutece.plugins.workflowcore.service.state.IStateService;
-import fr.paris.lutece.plugins.workflowcore.service.state.StateService;
-import fr.paris.lutece.portal.service.message.SiteMessageException;
 import fr.paris.lutece.portal.service.search.IndexationService;
 import fr.paris.lutece.portal.service.search.SearchIndexer;
-import fr.paris.lutece.portal.service.search.SearchItem;
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
 
 public class SolrFormsIndexer implements SolrIndexer
 {
-    private static final String FILTER_DATE_FORMAT = AppPropertiesService.getProperty( "forms-solr.index.date.format", "dd/MM/yyyy" );
     private static final int TAILLE_LOT = AppPropertiesService.getPropertyInt( "forms-solr.index.writer.commit.size", 100 );
 
 
@@ -101,42 +94,38 @@ public class SolrFormsIndexer implements SolrIndexer
     }
 
     @Override
-    public List<SolrItem> getDocuments( String arg0 )
+    public List<SolrItem> getDocuments( String idFormResponse )
     {
-    	List<SolrIndexerAction> indexerActionList = SolrIndexerActionHome.getList( FormsPlugin.getPlugin( ) );
     	List<SolrItem> solrItemList = new ArrayList< >( );
     	Map<Integer, Form> mapForms = FormHome.getFormList( ).stream( ).collect( Collectors.toMap( Form::getId, form -> form ) );
 		
-        for (SolrIndexerAction indexerAction : indexerActionList) 
+    	int idDocument = Integer.parseInt( idFormResponse );
+		FormResponse response = FormResponseHome.findByPrimaryKeyForIndex( idDocument );
+		Form form = mapForms.get( response.getFormId( ) );
+		State formResponseState = null;
+        if ( _stateService != null )
         {
-    		int idDocument = Integer.parseInt( indexerAction.getIdDocument( ) );
-    		FormResponse response = FormResponseHome.findByPrimaryKeyForIndex( idDocument );
-    		Form form = mapForms.get( response.getFormId( ) );
-    		State formResponseState = null;
-            if ( _stateService != null )
-            {
-                formResponseState = _stateService.findByResource( response.getId( ), FormResponse.RESOURCE_TYPE, form.getIdWorkflow( ) );
-            }
-            else
-            {
-                formResponseState = new State( );
-                formResponseState.setId( -1 );
-                formResponseState.setName( StringUtils.EMPTY );
-            }
-    		SolrItem solrItem = null;
-    		try
-            {
-                solrItem = getSolrItem( response, form, formResponseState );
-            }
-            catch( Exception e )
-            {
-                IndexationService.error( (SearchIndexer) this, e, null );
-            }
-    		if (solrItem != null)
-    		{
-    			solrItemList.add(solrItem);
-    		}
+            formResponseState = _stateService.findByResource( response.getId( ), FormResponse.RESOURCE_TYPE, form.getIdWorkflow( ) );
         }
+        else
+        {
+            formResponseState = new State( );
+            formResponseState.setId( -1 );
+            formResponseState.setName( StringUtils.EMPTY );
+        }
+		SolrItem solrItem = null;
+		try
+        {
+            solrItem = getSolrItem( response, form, formResponseState );
+        }
+        catch( Exception e )
+        {
+            IndexationService.error( (SearchIndexer) this, e, null );
+        }
+		if (solrItem != null)
+		{
+			solrItemList.add(solrItem);
+		}
         return solrItemList;
     }
 
@@ -179,16 +168,30 @@ public class SolrFormsIndexer implements SolrIndexer
     @Override
     public List<String> indexDocuments( )
     {
-        List<String> errors = new ArrayList<>( );
+        List<String> errors = new ArrayList<>();
+
         try
-        {
-            indexFormsResponse( );
-        }
-        catch( IOException | SiteMessageException e )
-        {
-            AppLogService.error( "Error indexing FormResponses", e );
-            errors.add( e.toString( ) );
-        }
+            {
+                List<FormResponse> formResponsesList = new ArrayList<>( TAILLE_LOT );
+                for ( FormResponse response : FormResponseHome.selectAllFormResponses() )
+                {
+                    if ( response != null )
+                    {
+                    	formResponsesList.add( response );
+                    }
+                    if ( formResponsesList.size( ) == TAILLE_LOT )
+                    {
+                        indexFormResponseList( formResponsesList );
+                        formResponsesList.clear( );
+                    }
+                }
+                indexFormResponseList( formResponsesList );
+            }
+            catch( Exception e )
+            {
+                AppLogService.error( e.getMessage( ), e );
+                errors.add(e.toString());
+            }
         return errors;
     }
 
@@ -196,34 +199,6 @@ public class SolrFormsIndexer implements SolrIndexer
     public boolean isEnable( )
     {
         return "true".equalsIgnoreCase( AppPropertiesService.getProperty( Utilities.PROPERTY_INDEXER_ENABLE ) );
-    }
-
-    public void indexFormsResponse( ) throws IOException, SiteMessageException
-    {
-        List<Integer> listFormResponsesId = FormResponseHome.selectAllFormResponsesId( );
-
-        try
-            {
-                List<FormResponse> listFormResponses = new ArrayList<>( TAILLE_LOT );
-                for ( Integer nIdFormResponse : listFormResponsesId )
-                {
-                    FormResponse response = FormResponseHome.findByPrimaryKeyForIndex( nIdFormResponse );
-                    if ( response != null )
-                    {
-                        listFormResponses.add( response );
-                    }
-                    if ( listFormResponses.size( ) == TAILLE_LOT )
-                    {
-                        indexFormResponseList( listFormResponses );
-                        listFormResponses.clear( );
-                    }
-                }
-                indexFormResponseList( listFormResponses );
-            }
-            catch( Exception e )
-            {
-                AppLogService.error( e.getMessage( ), e );
-            }
     }
 
     /**
@@ -255,7 +230,7 @@ public class SolrFormsIndexer implements SolrIndexer
             }
             catch( Exception e )
             {
-                IndexationService.error( (SearchIndexer) this, e, null );
+            	AppLogService.error("Error during indexation", e);
             }
 
             if ( solrItem != null )
@@ -315,7 +290,6 @@ public class SolrFormsIndexer implements SolrIndexer
         solrItem.addDynamicField( FormResponseSearchItem.FIELD_ID_FORM_RESPONSE, String.valueOf( nIdFormResponse ) );
         
         // --- field contents
-        solrItem.addDynamicField( SearchItem.FIELD_CONTENTS, manageNullValue( getContentToIndex( formResponse ) ) );
         solrItem.setContent(manageNullValue( getContentToIndex( formResponse ) ));
 
         // --- form title
@@ -381,7 +355,7 @@ public class SolrFormsIndexer implements SolrIndexer
             }
             catch( Exception e )
             {
-                AppLogService.error( "Unable to parse {} with date formatter {}", response.getResponseValue( ), FILTER_DATE_FORMAT, e );
+                AppLogService.error( "Unable to parse {}", response.getResponseValue( ), e );
             }
         }
         else
